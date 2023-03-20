a84b6ef2a59bef8042020f6373e1895db43786e7
Filip <filip.peszke.programista@gmail.com>
2023-03-20 20:06:04 +0100

    First commit

---
+++ DrawingApp/app/src/main/java/eu/fpeszke/drawingapp/DrawingView.kt
@@ -0,0 +1,4 @@
+package eu.fpeszke.drawingapp
+
+class DrawingView {
+}
---
+++ MyQuizApp/app/src/main/java/eu/fpeszke/myquizapp/MainActivity.kt
@@ -0,0 +1,29 @@
+package eu.fpeszke.myquizapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnStart : Button = findViewById(R.id.btn_start)
+        val etName : EditText = findViewById(R.id.et_name)
+        btnStart.setOnClickListener {
+
+            if(etName.text.isNotEmpty()){
+                val intent = Intent(this, QuizQuestionsActivity::class.java)
+                intent.putExtra(Constants.USER_NAME, etName.text.toString())
+                startActivity(intent)
+                finish()
+            } else {
+                Toast.makeText(this, "Please enter your name", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+}
